d72443298ffd55a3e829d23eeb83b51461963037
Sektor <krzysiek1230@gmail.com>
2024-01-13 18:38:29 +0100

    Minor changes

--- nvim/.config/nvim/lua/sektor/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/sektor/plugins/nvim-tree.lua
@@ -21,8 +21,8 @@ return {
 
         -- toggle file explorer
         vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-        vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-        vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })   -- refresh file explorer
+        vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file tree" }) -- collapse file explorer
+        vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file tree" })   -- refresh file explorer
 
         local tree = require('nvim-tree')
 
