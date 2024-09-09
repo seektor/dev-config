85c99082e4f632b388941da7466fbbe903d99680
Sektor <krzysiek1230@gmail.com>
2024-09-09 10:54:35 +0200

    Mason

---
+++ nvim/.config/nvim/lua/sektor/lazy/plugins/mason.lua
@@ -0,0 +1,32 @@
+return {
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+
+        "mfussenegger/nvim-dap",
+        "jay-babu/mason-nvim-dap.nvim",
+    },
+    config = function()
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool_installer = require("mason-tool-installer")
+        local mason_nvim_dap = require("mason-nvim-dap")
+
+        mason.setup({})
+
+        mason_lspconfig.setup({
+            ensure_installed = {},
+            automatic_installation = true,
+        })
+
+        mason_tool_installer.setup({
+            ensure_installed = {},
+        })
+
+        mason_nvim_dap.setup({
+            ensure_installed = {}
+        })
+    end,
+}
