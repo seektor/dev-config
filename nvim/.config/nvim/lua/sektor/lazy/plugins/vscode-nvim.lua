eb3dcf97c37a867e7031d58581bd6c770e147111
Sektor <krzysiek1230@gmail.com>
2025-05-14 19:04:25 +0200

    Dev tweaks

--- nvim/.config/nvim/lua/sektor/lazy/plugins/vscode-nvim.lua
+++ nvim/.config/nvim/lua/sektor/lazy/plugins/vscode-nvim.lua
@@ -4,12 +4,17 @@ return {
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             local vscode = require('vscode')
-            local c = vscode.colors
 
             vscode.load()
 
+            local c = require('vscode.colors').get_colors()
+
             vim.api.nvim_set_hl(0, "FloatBorder", vim.api.nvim_get_hl_by_name('TelescopePromptBorder', true))
             vim.api.nvim_set_hl(0, "NormalFloat", vim.api.nvim_get_hl_by_name('TelescopeNormal', true))
+            vim.api.nvim_set_hl(0, "SpellBad", { fg = c.vscLightRed, sp = c.vscDarkYellow, undercurl = true })
+            -- vim.api.nvim_set_hl(0, "SpellCap", { fg = c.vscYellow, undercurl = true })
+            -- vim.api.nvim_set_hl(0, "SpellRare", { fg = c.vscYellow, undercurl = true })
+            -- vim.api.nvim_set_hl(0, "SpellLocal", { fg = c.vscYellow, undercurl = true })
         end,
     }
 }
