2fe963ca56dbd3ae24d5833b110dba597e30d899
Sektor <krzysiek1230@gmail.com>
2026-04-26 20:11:29 +0200

    feat(nvim): add VS Code-style glyphs and indent markers to nvim-tree

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -22,12 +22,46 @@ return {
       },
       renderer = {
         group_empty = true,
+        indent_markers = {
+          enable = true,
+          icons = {
+            corner = "└",
+            edge = "│",
+            item = "│",
+            bottom = "─",
+            none = " ",
+          },
+        },
         icons = {
           show = {
             file = true,
             folder = true,
             folder_arrow = true,
             git = true,
+          },
+          glyphs = {
+            default = "",
+            symlink = "",
+            bookmark = "󰆤",
+            folder = {
+              arrow_closed = "",
+              arrow_open = "",
+              default = "󰉋",
+              open = "󰝰",
+              empty = "󰉖",
+              empty_open = "󰷏",
+              symlink = "󰉒",
+              symlink_open = "󰉒",
+            },
+            git = {
+              unstaged = "✎",
+              staged = "✓",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "★",
+              deleted = "",
+              ignored = "◌",
+            },
           },
         },
       },
