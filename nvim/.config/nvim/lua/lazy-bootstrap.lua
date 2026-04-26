a6ebd13ae3248368504f0c4920fab0209ace033c
Sektor <krzysiek1230@gmail.com>
2026-04-26 15:20:15 +0200

    feat(nvim): add catppuccin mocha colorscheme

--- nvim/.config/nvim/lua/lazy-bootstrap.lua
+++ nvim/.config/nvim/lua/lazy-bootstrap.lua
@@ -13,6 +13,18 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({}, {
+require("lazy").setup({
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    opts = { flavour = "mocha" },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}, {
   change_detection = { notify = false },
 })
