31432768ad657dcff1bd877db00df8ad78acf7e0
Sektor <krzysiek1230@gmail.com>
2026-04-27 13:33:45 +0200

    feat(nvim): add conform.nvim for auto-formatting on save
    Configure conform.nvim to format Python files with ruff_format and black
    on BufWritePre, falling back to LSP if no formatter is available.

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/conform.nvim",
+  event = "BufWritePre",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        python = { "ruff_format", "black" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+  end,
+}
