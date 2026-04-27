dc6facb1e83b38dccc2ac22fe3f845659470e21a
Sektor <krzysiek1230@gmail.com>
2026-04-27 13:27:09 +0200

    feat(nvim): add LSP and completion plugins
    Add mason + mason-lspconfig for LSP server management (pyright auto-installed)
    with standard keymaps (gd, gr, K, rename, code action, diagnostics), and
    nvim-cmp with LuaSnip for insert-mode completion sourced from LSP, snippets,
    buffer, and path.

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright" },
+        automatic_installation = true,
+      })
+
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "References" })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+    end,
+  },
+}
