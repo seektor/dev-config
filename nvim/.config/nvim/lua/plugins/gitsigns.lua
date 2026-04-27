b24350b7e3e0800b7d607ccdb0fffb34bd20df06
Sektor <krzysiek1230@gmail.com>
2026-04-27 13:37:27 +0200

    feat(nvim): add autopairs, comment, and gitsigns plugins
    Add nvim-autopairs integrated with cmp, Comment.nvim for easy toggling, and
    gitsigns.nvim with hunk navigation, staging, blame, and diff keymaps.

---
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,34 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("gitsigns").setup({
+      signs = {
+        add          = { text = "▎" },
+        change       = { text = "▎" },
+        delete       = { text = "" },
+        topdelete    = { text = "" },
+        changedelete = { text = "▎" },
+      },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local map = function(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+        end
+
+        map("n", "]h", gs.next_hunk, "Next hunk")
+        map("n", "[h", gs.prev_hunk, "Prev hunk")
+        map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+        map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+        map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage hunk")
+        map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset hunk")
+        map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
+        map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
+        map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
+        map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame line")
+        map("n", "<leader>tb", gs.toggle_current_line_blame, "Toggle line blame")
+        map("n", "<leader>hd", gs.diffthis, "Diff this")
+      end,
+    })
+  end,
+}
