aaa5d70e42ef5b10d88c9074a494cfabd839e3e0
Sektor <krzysiek1230@gmail.com>
2026-05-13 13:18:19 +0200

    refactor(nvim): split mason and lsp into separate plugin files

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,39 +1,30 @@
 return {
-  {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
     "williamboman/mason.nvim",
-    build = ":MasonUpdate",
-    config = function()
-      require("mason").setup()
-    end,
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
   },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  config = function()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      require("mason-lspconfig").setup({
-        ensure_installed = { "pyright", "ruff", "clangd" },
-        automatic_installation = true,
-        handlers = {
-          function(server_name)
-            require("lspconfig")[server_name].setup({ capabilities = capabilities })
-          end,
-        },
-      })
+    require("mason-lspconfig").setup({
+      ensure_installed = { "pyright", "ruff", "clangd" },
+      automatic_installation = true,
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup({ capabilities = capabilities })
+        end,
+      },
+    })
 
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "References" })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
-    end,
-  },
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "References" })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+  end,
 }
