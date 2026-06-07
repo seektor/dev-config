209a07be8e64dd4e78c154f08ce8b8ff2a4daea6
Sektor <krzysiek1230@gmail.com>
2026-06-07 11:11:14 +0200

    refactor(scripts,nvim): inline tmux-start, lazy-load telescope
    Inline the tmux-start shell script directly into the ^T keybinding
    function in keybindings.zsh, removing the external script file.
    Also add cmd = "Telescope" to defer telescope loading until first use.

--- scripts/.config/scripts/keybindings.zsh
+++ scripts/.config/scripts/keybindings.zsh
@@ -4,8 +4,10 @@ zle -N _cd_bookmark
 bindkey '^B' _cd_bookmark
 
 _tmux_start() {
-  ~/.config/scripts/tmux-start/tmux-start
-  zle reset-prompt
+  tmux new-session -d -s main -c "$(pwd)" 2>/dev/null
+  tmux new-session -d -s dev-config -c ~/Programming/__DevOps__/dev-config 2>/dev/null
+  BUFFER="tmux attach -t main"
+  zle accept-line
 }
 zle -N _tmux_start
 bindkey '^T' _tmux_start
