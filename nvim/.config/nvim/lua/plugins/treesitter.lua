b2c515929d1f6a1b03faea2e847e6c3b0f2da9da
Sektor <krzysiek1230@gmail.com>
2026-06-24 12:09:37 +0200

    refactor(nvim): migrate treesitter config to new main-branch API
    nvim-treesitter dropped setup()/highlight.enable in favor of
    install() + vim.treesitter.start(), and textobjects moved its
    select/move config and keymaps out of the treesitter setup table
    into its own plugin spec.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,39 +1,47 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     config = function()
-      require("nvim-treesitter").setup({
-        ensure_installed = {
-          "lua", "vim", "vimdoc",
-          "c", "cpp",
-          "javascript", "typescript", "tsx",
-          "python", "bash", "json", "yaml", "toml",
-          "html", "css", "markdown",
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = { ["]f"] = "@function.outer" },
-            goto_previous_start = { ["[f"] = "@function.outer" },
-          },
-        },
+      require("nvim-treesitter").install({
+        "lua", "vim", "vimdoc",
+        "c", "cpp",
+        "javascript", "typescript", "tsx",
+        "python", "bash", "json", "yaml", "toml",
+        "html", "css", "markdown",
       })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "*",
+        callback = function() pcall(vim.treesitter.start) end,
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    lazy = false,
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        move = { set_jumps = true },
+      })
+      vim.keymap.set({ "x", "o" }, "af", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "if", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "]f", function()
+        require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "[f", function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+      end)
     end,
   },
 }
